f79d0116d66faec6373bf75c10d7b913d093c75f
Sayed Ali <sydalwedaie@Sayeds-Air.home>
2022-03-19 15:12:23 +0300

    Change to fetch first.

--- sw.js
+++ sw.js
@@ -461,7 +461,7 @@ self.addEventListener('fetch', function (event) {
 		// Try the cache
 		caches.match(event.request).then(function (response) {
 			// return it if there is a response,or else fetch again
-			return response || fetch(event.request);
+			return fetch(event.request) || response;
 		})
 	);
 });
