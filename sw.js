a409634cecadd1016bb34dc0948a0c390ccd390a
Sayed Ali <sydalwedaie@Sayeds-MacBook-Air.local>
2022-09-05 16:40:29 +0300

    Limit iOS splash meta tags. Edit font sizes.

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ const other = [
 
 const all_assets = [...nnc, ...nc, ...checklist_content, ...info, ...fonts, ...other]
 
-var cacheName = "ecl-cache-003"
+var cacheName = "ecl-cache-004"
 self.addEventListener('install', function (event) {
 
 	event.waitUntil((async () => {
