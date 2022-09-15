be7f2a9f56d4fc6553b6d42f9aa6642c653b4c41
Sayed Ali <sydalwedaie@Sayeds-MacBook-Air.local>
2022-09-16 02:52:04 +0300

    Update cache name

--- sw.js
+++ sw.js
@@ -38,7 +38,7 @@ const other = [
 
 const all_assets = [...nnc, ...nc, ...checklist_content, ...info, ...fonts, ...content_images, ...scripts,  ...other]
 
-var cacheName = "ecl-cache-008"
+var cacheName = "ecl-cache-009"
 self.addEventListener('install', function (event) {
 
 	event.waitUntil((async () => {
